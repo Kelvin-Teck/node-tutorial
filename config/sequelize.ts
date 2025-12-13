1baf8aa705fe27d3cb5dd0028f726bfd652b4e0a
Eneh Kelvin Chukwuemeka <kelvineneh4@gmail.com>
2025-12-13 15:56:44 +0100

    ,,..

--- config/sequelize.ts
+++ config/sequelize.ts
@@ -2,6 +2,8 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const sequelize = new Sequelize(
   process.env.DATABASE_NAME,
   process.env.DATABASE_USER,
@@ -10,9 +12,16 @@ const sequelize = new Sequelize(
     host: process.env.DATABASE_HOST,
     dialect: "postgres",
     port: Number(process.env.DATABASE_PORT),
-    logging: true,
+    logging: console.log,
+    ...(isProduction && {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    }),
   }
-  
 );
 
 export default sequelize;
